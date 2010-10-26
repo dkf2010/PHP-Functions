4d9bfc828642705be602a05dc3597544e6ad1bbc
Dennis Kruemmel <dennis@kruemmel.it>
2010-10-26 16:32:53 +0200

    Parameterbehandlung verbessert

--- email_protect.php
+++ email_protect.php
@@ -1,5 +1,8 @@
 <?php
-function email_protect($email, $text) {
+function email_protect() {
+	$email = func_num_args()>0 ? func_get_arg(0) : "mail@example.com";
+	$text  = func_num_args()>1 ? func_get_arg(1) : "";
+
 	$character_set = '+-.0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
 	
 	$key = str_shuffle($character_set); $cipher_text = ''; $id = 'e'.rand(1,999999999);
@@ -11,11 +14,16 @@ function email_protect($email, $text) {
 	$script.= 'for(var e=0;e<c.length;e++)d+=b.charAt(a.indexOf(c.charAt(e)));';
 	
 	$script.= 'document.getElementById("'.$id.'").innerHTML="<a href=\\"mailto:"+d+"\\">';
+
 	if(empty($text)) {
 		$script.='"+d+"';
 	} else {
 		$script.=$text;
 	}
+	
+	
+	
+	
 	$script.= '</a>"';
 	
 	$script = "eval(\"".str_replace(array("\\",'"'),array("\\\\",'\"'), $script)."\")"; 
