f7ad66cb7c801abc398dd0ab51b93b1a623ce177
Dennis Kruemmel <dennis@kruemmel.it>
2010-10-26 17:06:42 +0200

    Revert "added the usage description"
    This reverts commit 17b0f3e02afdaba5f14fdf1a99f1a9d810a9921a.

--- email_protect.php
+++ email_protect.php
@@ -1,19 +1,5 @@
 <?php
 function email_protect() {
-	/*
-	Usage:
-	
-	email_protect()
-	returns a protected dummy email address
-	
-	email_protect('myemail@address.com')
-	returns the protected email address 'myemail@address.com' and the protected link description of 'myemail@address.com'
-	
-	email_protect('myemail@address.com', 'My Link Text')
-	returns the protected email address 'myemail@address.com' and the link description 'My Link Text'
-	
-	*/
-	
 	$email = func_num_args()>0 ? func_get_arg(0) : "mail@example.com";
 	$text  = func_num_args()>1 ? func_get_arg(1) : "";
 
@@ -24,15 +10,24 @@ function email_protect() {
 	for ($i=0;$i<strlen($email);$i+=1) $cipher_text.= $key[strpos($character_set,$email[$i])];
 	
 	$script = 'var a="'.$key.'";var b=a.split("").sort().join("");var c="'.$cipher_text.'";var d="";';
+	
 	$script.= 'for(var e=0;e<c.length;e++)d+=b.charAt(a.indexOf(c.charAt(e)));';
+	
 	$script.= 'document.getElementById("'.$id.'").innerHTML="<a href=\\"mailto:"+d+"\\">';
-		if(empty($text)) {
-			$script.='"+d+"';
-		} else {
-			$script.=$text;
-		}
+
+	if(empty($text)) {
+		$script.='"+d+"';
+	} else {
+		$script.=$text;
+	}
+	
+	
+	
+	
 	$script.= '</a>"';
+	
 	$script = "eval(\"".str_replace(array("\\",'"'),array("\\\\",'\"'), $script)."\")"; 
+	
 	$script = '<script type="text/javascript">/*<![CDATA[*/'.$script.'/*]]>*/</script>';
 	
 	return '<span id="'.$id.'">[javascript protected email address]</span>'.$script;
